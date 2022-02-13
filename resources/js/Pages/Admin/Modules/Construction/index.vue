7bd4fbf839f10dac6a525397b5680c011bae6e7e
Gustavo Santaella <llxsantaellaxll@gmail.com>
2022-02-13 19:38:44 -0400

    quitando el true

--- resources/js/Pages/Admin/Modules/Construction/index.vue
+++ resources/js/Pages/Admin/Modules/Construction/index.vue
@@ -59,7 +59,7 @@
           </td>
           <td>
             <button
-            v-show="this.$page.props.flash.userManagement.construction || true"
+            v-show="this.$page.props.flash.userManagement.construction"
               @click="this.redirectOnEdi(construction)"
               class="btn btn-sm btn-primary"
             >
