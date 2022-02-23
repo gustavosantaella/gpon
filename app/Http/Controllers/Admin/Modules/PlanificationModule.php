0e04c7ff7865f485792bf4fff7a23d8db3c35184
Gustavo Alejandro <51528255+gustavosantaella@users.noreply.github.com>
2022-02-23 10:50:40 -0400

    withTrashed

--- app/Http/Controllers/Admin/Modules/PlanificationModule.php
+++ app/Http/Controllers/Admin/Modules/PlanificationModule.php
@@ -139,7 +139,7 @@ class PlanificationModule extends BaseController
             $users = (User::whereRelation('roles', 'name', 'SUPER USUARIO')->get());
             $message  = "Se ha subido la documentacion de '$planification->name'";
             dd($planification->construction);
-          if(!$planification->construction){
+          if(!$planification->construction->withTrashed()){
 
                 $message  = "Se ha subido la documentacion y hecho el pase a construccion de '$planification->name'";
                 $managements = $this->model('management')->whereConstruction(true)->get()->map(function ($value, $key) {
